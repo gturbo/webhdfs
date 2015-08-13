2af0cbc5629b459aa887b39b09e711e6d86073fa
gael urbauer <gael.urbauer@bred.fr>
2015-08-13 18:06:59 +0200

    fixed upload selected files

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var request = require('request');
 var util = require('util');
 var fs = require('fs');
 var extend = util._extend;
+var async = require("async");
 /* GET home page. */
 
 var baseHDFS = 'https://bi-hadoop-prod-2059.services.dal.bluemix.net:8443/gateway/default/webhdfs/v1/';
@@ -17,16 +18,16 @@ var baseOptions = {
 }
 
 var hdfs = function (options, callback) {
-	options=extend(baseOptions, options);
+	options = extend(baseOptions, options);
 	console.log('options: \n' + util.inspect(options));
 	if (callback)
 		request(options, function (e, r, b) {
-		if (e) {
-			console.log(e);
-		}
-		callback(r, b, e);
-	});
-	else 
+			if (e) {
+				console.log(e);
+			}
+			callback(r, b, e);
+		});
+	else
 		return request(options);
 }
 
@@ -50,9 +51,7 @@ router.get(/^\/list\/*(.*)?$/, function (req, res, next) {
 	});
 });
 
-var getLocalFileList = function (urlParam, callback) {
-	console.log(util.inspect(req.params));
-	var path = urlParam;
+var getLocalFileList = function (path, callback) {
 	console.log('final path: ' + path);
 	fs.readdir(path, function (err, files) {
 		if (err) {
@@ -119,10 +118,10 @@ router.get(/^\/loc-list\/*(.*)?$/, function (req, res, next) {
 hdfsUpload = function (localPath, localFileName, distPath, distFileName, callback) {
 	if (!distFileName || distFileName.length == 0)
 		distFileName = localFileName;
-	
+
 	if (distPath.slice(-1) != '/')
-		distPath+='/';
-		
+		distPath += '/';
+
 	// first request returning a redirection to definitive upload url
 	hdfs({
 		method : 'PUT',
@@ -132,30 +131,56 @@ hdfsUpload = function (localPath, localFileName, distPath, distFileName, callbac
 		if (res1.statusCode == 307) {
 			console.log('definitive url:' + res1.headers.location);
 			// real file upload
-			fs.createReadStream(localPath + localFileName).pipe(
-				hdfs({
-					method : 'PUT',
-					uri : res1.headers.location + '?op=CREATE',
-				}).on('end', function () {
-					callback(localFileName);
-				}).on('error', function (err) {
-
-					console.log('error while uploading file\n', util.inspect(err));
-				}));
+			fName = localPath + localFileName
+				fs.createReadStream(fName).on('error', function (err) {
+					console.log('error opening file ' + fName);
+					callback(err);
+				}).pipe(
+					hdfs({
+						method : 'PUT',
+						uri : res1.headers.location + '?op=CREATE',
+					}).on('end', function () {
+						callback();
+					}).on('error', function (err) {
+						console.log('error while uploading file\n', util.inspect(err));
+						callback(err);
+					}));
 		} else {
 			console.log('error in upload for url: ' + '\n' + res1.body);
+			callback(err);
 		}
 	});
 }
 // upload files
 router.post(/^\/upload\/*(.*)?$/, function (req, res, next) {
-	console.log(req.body);
-	hdfsUpload('e:/tmp/', 'coucou.txt', basePath, 'hello.txt', function(file) {res.send('<h2>FILE: ' + file +' UPLOAD OK</h2>');res.end();});
+	console.log('upload body:           ***************************\n' + util.inspect(req.body));
+/*
+	hdfsUpload('e:/tmp/', 'coucou3.txt', basePath, 'hello.txt', function () {
+		res.send(['coucou3.txt']);
+		res.end();
+	});
 	return;
+	*/
 	getLocalFileList(req.params['0'], function (files, err) {
-		if (files) {
-			res.send(files);
-			res.end();
+		if (files && files.length) {
+			var i = files.length,
+			distPath = req.body.path,
+			distFileName = null;
+			var locPath = req.params['0'].split('/');
+			locPath.pop();
+			locPath = locPath.join('/') + '/';
+			var uploadedFiles = [];
+			async.parallel(files.map(function(file) {
+				hdfsUpload(locPath, file, distPath, file, function(err) {
+					if (!err)
+						uploadedFiles.push(file);
+					})
+			}), function (err){
+				if (err)
+					console.log(err);
+				res.send(uploadedFiles);
+				res.end();
+			});
 		} else
 			res.send('PAS DE FICHIER SELECTIONNE<br><br><br>VERIFIEZ LE CHEMIN LOCAL');
 	});
